62aab554bec5e1652c71c24c6149d6785880f6af
Felipe David <felipedavid.huh@gmail.com>
2024-06-28 05:42:08 -0300

    making requests

--- main.go
+++ main.go
@@ -1,22 +1,99 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"net"
+	"os"
 	"strings"
 )
 
 func main() {
-	url, err := parseURL("http://google.com")
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: ./puppy <url>\n\n")
+		return
+	}
+
+	url, err := parseURL(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", url)
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", url.Host, url.Port))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	var req string
+	req += fmt.Sprintf("GET %s HTTP/1.0\r\n", url.Path)
+	req += fmt.Sprintf("Host: %s\r\n", url.Host)
+	req += "\r\n"
+
+	_, err = conn.Write([]byte(req))
+	if err != nil {
+		panic(err)
+	}
+
+	var res Response
+	res.Headers = make(map[string]string, 0)
+
+	resReader := bufio.NewReader(conn)
+
+	statusLine, _, err := resReader.ReadLine()
+	if err != nil {
+		panic(err)
+	}
+
+	statusLineWords := strings.Split(string(statusLine), " ")
+	res.Version = statusLineWords[0]
+	res.Status = statusLineWords[1]
+	res.Explanation = statusLineWords[2]
+
+	for {
+		buf, _, err := resReader.ReadLine()
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			panic(err)
+		}
+		line := string(buf)
+
+		if line == "\r\n" {
+			break
+		}
+
+		headerValue := strings.Split(line, ":")
+		header := headerValue[0]
+		value := strings.TrimSpace(strings.Join(headerValue[1:], ""))
+
+		res.Headers[header] = value
+	}
+
+	body, err := io.ReadAll(resReader)
+	if err != nil && err.Error() != "EOF" {
+		panic(err)
+	}
+
+	res.Body = string(body)
+
+	fmt.Printf("%+v", res)
+}
+
+type Response struct {
+	Version     string
+	Status      string
+	Explanation string
+	Headers     map[string]string
+	Body        string
 }
 
 type URL struct {
 	Scheme string
 	Host   string
+	Port   string
 	Path   string
 }
 
@@ -24,9 +101,12 @@ func parseURL(urlStr string) (*URL, error) {
 	var url URL
 
 	schemeAndRest := strings.Split(urlStr, "://")
-	url.Scheme = schemeAndRest[0]
-
-	urlStr = schemeAndRest[1]
+	if len(schemeAndRest) == 2 {
+		url.Scheme = schemeAndRest[0]
+		urlStr = schemeAndRest[1]
+	} else {
+		url.Scheme = "http"
+	}
 
 	if !strings.Contains(urlStr, "/") {
 		urlStr += "/"
@@ -34,7 +114,19 @@ func parseURL(urlStr string) (*URL, error) {
 
 	hostAndPath := strings.Split(urlStr, "/")
 	url.Host = hostAndPath[0]
-	url.Path = hostAndPath[1]
+	if len(hostAndPath) >= 2 {
+		url.Path = "/" + strings.Join(hostAndPath[1:], "/")
+	} else {
+		url.Path = "/"
+	}
+
+	if strings.Contains(url.Host, ":") {
+		hostAndPort := strings.Split(url.Host, ":")
+		url.Host = hostAndPort[0]
+		url.Port = hostAndPort[1]
+	} else {
+		url.Port = "80"
+	}
 
 	return &url, nil
 }
